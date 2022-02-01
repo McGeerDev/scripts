854afe4a801231f8d844b2944d934a5998f27cfa
mr_bliss <devan.mcgeer@zenaptix.com>
2022-02-01 11:10:03 +0200

    gitScripts: Added script to merge current branch to main/master

---
+++ mergeMain.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+currentBranch = `git branch --show-current`
+
+pull() {
+	echo `git pull`
+}
+pull();
+echo `git checkout main`;
+pull();
+echo `git checkout $currentBranch`;
+echo `git pull origin main`;
